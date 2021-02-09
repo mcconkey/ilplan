9de65b1a8d54819347ac529b4234baae8a477b42
Christopher McConkey <topher@ChristophersMBP.satx.rr.com>
2021-02-09 07:40:27 -0600

    sideMenu

--- src/SideMenuContent.js
+++ src/SideMenuContent.js
@@ -13,7 +13,7 @@ const SideMenuContent = () => {
               This application was created by me, Technical Sergeant Christopher McConkey, at my own 
                volition. The content of this website is not the product of the United States Air Force
                nor the Department of Defense. It has not been endorsed or approved by any government agency.  
-               This tool is offered freely without warrantee or guarantee.
+               This tool is offered freely without warranty or guarantee.
                 <br /><br />
                If you have questions, concerns or ideas please hit me up! (I'm in the GAL or use my personal email: <a href="mailto:mcconkey@gmail.com">mcconkey@gmail.com</a>.)
                 </div>
